0188ff5f06988b4f2c6b3284ecdc0233d3e33543
Andrew <andrewykin@gmail.com>
2022-12-10 07:42:08 -0800

    dev 3 adjust

--- charts.js
+++ charts.js
@@ -162,12 +162,11 @@ function buildCharts(sample) {
           },
           bar: {color: "black"},
           steps: [
-            {range: [0, 2], color: "red" },
-            {range: [2, 4], color: "orange" },
-            {range: [4, 6], color: "yellow" },
-            {range: [6, 8], color: "light green" },
-            {range: [8, 10], color: "green" }
-          ]
+            { range: [0, 2], color: "red" },
+            { range: [2, 4], color: "orange" },
+            { range: [4, 6], color: "yellow" },
+            { range: [6, 8], color: "lime" },
+            { range: [8, 10], color: "green" }]
         }
       }
     ];
