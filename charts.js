a6f034d6a90eeb63eb341dd933ef57ffc0c8f0c5
Andrew <andrewykin@gmail.com>
2022-12-10 07:37:14 -0800

    deliverable 3 update & deliverable 4 start

--- charts.js
+++ charts.js
@@ -166,8 +166,7 @@ function buildCharts(sample) {
             {range: [2, 4], color: "orange"},
             {range: [4, 6], color: "yellow"},
             {range: [6, 8], color: "light green"},
-            {range: [8, 10], color, "green"}
-          ]
+            {range: [8, 10], color, "green"}]
         }
       }
     ];
